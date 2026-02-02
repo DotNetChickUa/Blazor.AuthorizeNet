b6ed2959f073184c83828c4cb9b78be40d001e75
DotnetchickUa <DotnetchickUa@ukr.net>
2026-02-02 15:41:42 +0200

    Add project files.

---
+++ FormTokenGenerator/Program.cs
@@ -0,0 +1,61 @@
+using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Controllers.Bases;
+using Environment = AuthorizeNet.Environment;
+
+ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = Environment.SANDBOX;
+
+ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType
+{
+    name = "",
+    ItemElementName = ItemChoiceType.transactionKey,
+    Item = ""
+};
+
+var billingAddress = new customerAddressType
+{
+    firstName = "FirstName",
+    lastName = "LastName",
+    address = "Address",
+    city = "City",
+    zip = "Zip",
+    country = "Country",
+    email = "test@gmail.com",
+    state = "State",
+    phoneNumber = "PhoneNumber"
+};
+
+var transactionRequest = new transactionRequestType
+{
+    transactionType = nameof(transactionTypeEnum.authCaptureTransaction),
+    amount = 20,
+
+    customer = new customerDataType() { email = "test@gmail.com"},
+    billTo = billingAddress,
+    profile = new customerProfilePaymentType() { customerProfileId = "123" },
+};
+
+var hostedPaymentSettings = new settingType[]
+{
+            new settingType { settingName = "hostedPaymentReturnOptions", settingValue = "{\"showReceipt\": false, \"url\": \"https://36b539b956eb.ngrok-free.app/empty.html\", \"urlText\": \"Continue\"}" },
+            new settingType { settingName = "hostedPaymentButtonOptions", settingValue = "{\"text\": \"Pay\"}" },
+            new settingType { settingName = "hostedPaymentStyleOptions", settingValue = "{\"bgColor\": \"blue\"}" },
+            new settingType { settingName = "hostedPaymentPaymentOptions", settingValue = "{\"cardCodeRequired\": false, \"showCreditCard\": true, \"showBankAccount\": false}" },
+            new settingType { settingName = "hostedPaymentSecurityOptions", settingValue = "{\"captcha\": false}" },
+            new settingType { settingName = "hostedPaymentShippingAddressOptions", settingValue = "{\"show\": false, \"required\": false}" },
+            new settingType { settingName = "hostedPaymentBillingAddressOptions", settingValue = "{\"show\": true, \"required\": false}" },
+            new settingType { settingName = "hostedPaymentCustomerOptions", settingValue = "{\"showEmail\": false, \"requiredEmail\": false, \"addPaymentProfile\": true}" },
+            new settingType { settingName = "hostedPaymentOrderOptions", settingValue = "{\"show\": true, \"merchantName\": \"ACCU\"}" },
+            new settingType { settingName = "hostedPaymentIFrameCommunicatorUrl", settingValue = "{\"url\": \"https://36b539b956eb.ngrok-free.app/iframecommunicator.html\"}" } // TODO: Get from configuration
+};
+
+var request = new getHostedPaymentPageRequest
+{
+    transactionRequest = transactionRequest,
+    hostedPaymentSettings = hostedPaymentSettings
+};
+
+var controller = new getHostedPaymentPageController(request);
+var result = controller.ExecuteWithApiResponse();
+Console.WriteLine(result.token);
+Console.ReadLine();
